9d8472e8171abe6fe13304d1458d8e2fc9b4c3dc
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-02-20 20:42:25 +0330

    answer

--- folder_6/Removing-Elements.js
+++ folder_6/Removing-Elements.js
@@ -1,2 +1,11 @@
 // Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 
+function removeEveryOther(arr) {
+  let array = arr.filter((el, ind) => {
+    if (ind % 2 === 0) {
+      return el;
+    }
+  });
+  return array;
+}
+
